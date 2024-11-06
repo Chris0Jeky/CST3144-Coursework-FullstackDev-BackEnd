036837cd1c978b7e1618ffafc88768a02c22e5a4
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-06 10:08:57 +0000

    load env variables, and connect to mongodb

--- express-app/index.js
+++ express-app/index.js
@@ -1,3 +1,8 @@
+// Load environment variables
+require('dotenv').config();
+// And MongoDB
+const { MongoClient } = require('mongodb');
+
 // Import Express.js
 const express = require('express');
 const app = express();
@@ -8,7 +13,7 @@ app.use(express.json()); // This will parse JSON payloads
 // Middleware to log every request
 app.use((req, res, next) => {
   console.log(`${req.method} request for '${req.url}'`);
-  next(); // Move to the next middleware or route handler
+  next(); //
 });
 
 // Simple GET route for the root URL
@@ -25,3 +30,20 @@ app.get('/about', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+// Connection to MongoDB
+let db;
+
+MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
+    .then((client) => {
+      db = client.db(); // Select the database
+      console.log('Connected to MongoDB Atlas');
+
+      // Start the server after the database connection is ready
+      app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+      });
+    })
+    .catch((err) => {
+      console.error('Failed to connect to MongoDB Atlas', err);
+    });
