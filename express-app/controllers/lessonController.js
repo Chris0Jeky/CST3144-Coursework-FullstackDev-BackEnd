4539940ca1d29e7a37c8727f3dbde8f65cc2fb1a
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-13 11:17:58 +0000

    updating controller to use model

--- express-app/controllers/lessonController.js
+++ express-app/controllers/lessonController.js
@@ -1,19 +1,18 @@
 const { ObjectId } = require('mongodb');
+const { getLessonsCollection } = require('../models/lessonModel');
 
 function getAllLessons(req, res) {
-    const lessonsCollection = db.collection('lessons');
+    const db = req.app.locals.db; // Access db from app locals
+    const lessonsCollection = getLessonsCollection(db);
 
-    // Extract query parameters for filtering and sorting
     const { topic, location, price } = req.query;
-
     let filter = {};
+
     if (topic) filter.topic = topic;
     if (location) filter.location = location;
     if (price) filter.price = parseFloat(price);
 
-    lessonsCollection
-        .find(filter)
-        .toArray()
+    lessonsCollection.find(filter).toArray()
         .then((lessons) => {
             res.json(lessons);
         })
@@ -23,15 +22,15 @@ function getAllLessons(req, res) {
 }
 
 function updateLesson(req, res) {
-    const lessonsCollection = db.collection('lessons');
+    const db = req.app.locals.db;
+    const lessonsCollection = getLessonsCollection(db);
     const lessonId = req.params.id;
     const updateData = req.body;
 
-    lessonsCollection
-        .updateOne(
-            { _id: new ObjectId(lessonId) },
-            { $set: updateData }
-        )
+    lessonsCollection.updateOne(
+        { _id: new ObjectId(lessonId) },
+        { $set: updateData }
+    )
         .then((result) => {
             if (result.matchedCount === 0) {
                 res.status(404).json({ error: 'Lesson not found' });
