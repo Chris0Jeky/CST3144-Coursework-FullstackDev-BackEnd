8775986d5235fd658be9881482ca62be3e8b34d1
Chris0Jeky <jeky.tck@gmail.com>
2024-11-18 23:31:23 +0000

    solving issue with multiple listen()

--- index.js
+++ index.js
@@ -10,37 +10,34 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON payloads
 app.use(express.json());
 
-// cors
+// Enable CORS
 app.use(cors());
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
 
 // Middleware to log every request
 app.use((req, res, next) => {
-  console.log(`${req.method} request for '${req.url}'`);
-  next();
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
 });
 
 // Static file middleware for images
 app.use('/images', express.static('images', {
-  fallthrough: false, // Prevents moving to next middleware if file not found
+    fallthrough: false, // Prevents moving to next middleware if file not found
 }));
 
 // Error handling for static files
 app.use((err, req, res, next) => {
-  if (err) {
-    if (err.code === 'ENOENT') {
-      res.status(404).json({ error: 'Image not found' });
+    if (err) {
+        if (err.code === 'ENOENT') {
+            res.status(404).json({ error: 'Image not found' });
+        } else {
+            res.status(500).json({ error: 'Server error' });
+        }
     } else {
-      res.status(500).json({ error: 'Server error' });
+        next();
     }
-  } else {
-    next();
-  }
 });
 
+// Import routes
 const lessonRoutes = require('./routes/lessonRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
@@ -50,12 +47,12 @@ app.use('/', orderRoutes);
 
 // Simple GET route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello World! Welcome to Express.js');
+    res.send('Hello World! Welcome to Express.js');
 });
 
 // Another route
 app.get('/about', (req, res) => {
-  res.send('This is the About page');
+    res.send('This is the About page');
 });
 
 // Catch-all route for undefined endpoints
@@ -70,8 +67,6 @@ app.use((err, req, res, next) => {
 });
 
 // Connection to MongoDB
-let db;
-
 MongoClient.connect(process.env.MONGODB_URI)
     .then((client) => {
         const db = client.db(); // Select the database
