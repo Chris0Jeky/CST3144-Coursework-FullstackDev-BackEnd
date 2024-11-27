0e3a042607fa5e08b04df71ec8b542447c4e1592
Chris <59696583+Chris0Jeky@users.noreply.github.com>
2024-11-27 11:23:48 +0000

    added comment

--- routes/lessonRoutes.js
+++ routes/lessonRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const lessonController = require('../controllers/lessonController');
 
+// Route to get all lessons, possibly with filters
 router.get('/lessons', lessonController.getAllLessons);
+
+// Route to update a lesson by ID
 router.put('/lessons/:id', lessonController.updateLesson);
 
 module.exports = router;
